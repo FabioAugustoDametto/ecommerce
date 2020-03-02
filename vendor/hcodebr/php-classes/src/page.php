454ec23084b6697222caa2f303de19e551a309df
unknown <a.dametto@hotmail.com>
2020-03-01 21:38:26 -0300

    Tela de login

--- vendor/hcodebr/php-classes/src/page.php
+++ vendor/hcodebr/php-classes/src/page.php
@@ -9,6 +9,8 @@ class Page {
     private $tpl;
     private $options = [];
     private $defaults = [
+        "header" => true,
+        "footer" => true,
         "data" => []
     ];
 
@@ -17,7 +19,7 @@ class Page {
         $this->options = array_merge($this->defaults, $opts);
 
         $config = array(
-            "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/",
+            "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"].$tpl_dir,
             "cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
             "debug"         => false, // set to false to improve the speed
         );        
@@ -25,17 +27,21 @@ class Page {
 
         $this->tpl = new Tpl;
 
-        $this->setData($this->options["data"]);        
+        $this->setData($this->options["data"]);  
 
-        $this->tpl->draw("header");    }
+
+        if ($this->options["header"] === true) {
+             	$this->tpl->draw("header");
+             }     
+     
+
+           }
 
     private function setData($data = array()) {
         foreach ($data as $key => $value) {
             $this->tpl->assign($key,$value);
         }
     }
-
-
 
     public function setTpl($name, $data = array(), $returnHTML = false) {
         $this->setData($data);
@@ -44,7 +50,9 @@ class Page {
 
     public function __destruct() {
 
-        $this->tpl->draw("footer");
+        if ($this->options["footer"] === true) {
+             	$this->tpl->draw("footer");
+             }  
 
     }
 }
